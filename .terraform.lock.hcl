5fdaf12104710ea7677127a3951609dfd2882076
Ubuntu <ubuntu@ip-172-31-11-176.us-west-1.compute.internal>
2023-08-31 01:01:46 +0000

    inital commit with 2 docker instances and resource random_string added lesson 24

---
+++ .terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.5.1"
+  hashes = [
+    "h1:VSnd9ZIPyfKHOObuQCaKfnjIHRtR7qTw19Rz8tJxm+k=",
+    "zh:04e3fbd610cb52c1017d282531364b9c53ef72b6bc533acb2a90671957324a64",
+    "zh:119197103301ebaf7efb91df8f0b6e0dd31e6ff943d231af35ee1831c599188d",
+    "zh:4d2b219d09abf3b1bb4df93d399ed156cadd61f44ad3baf5cf2954df2fba0831",
+    "zh:6130bdde527587bbe2dcaa7150363e96dbc5250ea20154176d82bc69df5d4ce3",
+    "zh:6cc326cd4000f724d3086ee05587e7710f032f94fc9af35e96a386a1c6f2214f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b6d88e1d28cf2dfa24e9fdcc3efc77adcdc1c3c3b5c7ce503a423efbdd6de57b",
+    "zh:ba74c592622ecbcef9dc2a4d81ed321c4e44cddf7da799faa324da9bf52a22b2",
+    "zh:c7c5cde98fe4ef1143bd1b3ec5dc04baf0d4cc3ca2c5c7d40d17c0e9b2076865",
+    "zh:dac4bad52c940cd0dfc27893507c1e92393846b024c5a9db159a93c534a3da03",
+    "zh:de8febe2a2acd9ac454b844a4106ed295ae9520ef54dc8ed2faf29f12716b602",
+    "zh:eab0d0495e7e711cca367f7d4df6e322e6c562fc52151ec931176115b83ed014",
+  ]
+}
+
+provider "registry.terraform.io/kreuzwerker/docker" {
+  version     = "2.15.0"
+  constraints = "~> 2.15.0"
+  hashes = [
+    "h1:NEBNtKNukqr6qk5vWu8Xx/nHBVNNdlty0hrFG76K7AE=",
+    "zh:0241e5c7b66c14aa54e367dfe380fbde8388d3254cbe8a70717c12f71897e82b",
+    "zh:0f162f0a01ffe9eec32e78dfc2a5b05a373230c41f041439efa3f4b68903fdcb",
+    "zh:1c222c1425fbb0367154bcb8e4d87b19d6eae133fbb341f73102fa3b300f34bf",
+    "zh:679206433e31d8fa69d95167af6d2cd762218e89811b735ee20bd8da19f97854",
+    "zh:a16baab045bc7a709a9767816304cc5548aa2ee0b72c0eee49e3826e6a46a3fd",
+    "zh:a29c4e304a6a7faf5b651a61a91a1aa2e837591cff049fbe1c747b6319e43956",
+    "zh:bed7a69dbb9a024aecfac840ca2ac2f1527062f3d9c8c718e4e81d464b5ad329",
+    "zh:c22aa10806de216b6aa0b36a2c1114a9fdaa5b47925aaad3333de3ce24cc52c9",
+    "zh:d034295663d8a39035fd6fdf0488b72544b13f48acaee797af967343248676f8",
+    "zh:d9001dfeac0db1799d8ab9d04be090522529baae0dba7f7e82b71f2168f05582",
+    "zh:d9f3eb7ef8f256eb9148d72bd4a00e34f3be5570484598e26b603cbdc5eed923",
+    "zh:ef573f1e0f000127fa9f569c8ee320f03ba1d302a6fbf9aac8300efa6fc6f4b2",
+    "zh:ef7e246b4205202161b4119c10a1282f74243e029508184132731f9f6f6d9f4c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,129 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      # this is the api for docker that we will use.
+      #version = "2.12.0"
+      #version = "2.15.0"
+      # this is a test version and will be blocked by the .terraform.lock.hcl file.
+      # my setup is currently at 
+      # version = 3.0.2
+      # to get the 2.15.0 to take must do a    terraform init -upgrade
+      
+      # to prevent an upgrade on second number do this:
+      #  version = "~> 2.12.0"  this will allow any 2.12.x but not 2.15
+      
+      # version  = "~> 2.12" will allow 2.x but not 3.x.  Rightmost number can increase as much as possible without next left number incrementing.
+      
+      # version = "~> 2.15.0" will lock it to 2.15.x 
+      version = "~> 2.15.0"
+      
+    }
+  }
+}
+
+provider "docker" {}
+# this instantiates the docker provider itself
+
+
+# https://registry.terraform.io/providers/kreuzwerker/docker/2.15.0/docs/resources/image
+resource "docker_image" "nodered_image" {
+  # name of the image itself. This is the docker hub name reference not an arbitrary name that we are assigning.
+  name = "nodered/node-red:latest"
+}
+
+
+
+# use random resource to generate unique names for the multi-container deployment
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
+
+
+
+# deployment of the container based upon the image  "nodered_image" above
+# "docker_container" must be used. That cannot be changed.
+# https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container#nestedblock--ports
+resource "docker_container" "nodered_container" {
+  name = "nodered"
+  # this is a logical value for referencing only.
+  
+  
+  image = docker_image.nodered_image.latest
+  # this is referenced from the docker image above: docker_image.nodered_image.latest
+  # "latest" can be referenced from other resources as a single image.  This seems to work only with docker 2.15.0
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image
+  
+  # for docker 3.0.2 use 
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#read-only
+  # image = docker_image.nodered_image.image_id
+  ##image = docker_image.nodered_image.image_id
+
+  # ports will need to be exposed on this container
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container#nestedblock--ports
+  # internal (Number) Port within the container.; external (Number) Port exposed out of the container. If not given a free random port >= 32768 will be used.
+  # ip (String) IP address/mask that can access this port. Defaults to 0.0.0.0.; protocol (String) Protocol that can be used over this port. Defaults to tcp.
+  ports {
+    # ports is a nested schema
+    internal = 1880
+    # terraform refers to this as a number and not an integer.  1880 is the nodered listening port.  See documentation
+    #external = 1880
+    # if we comment out the external port, docker will automatically choose external port for you
+  }
+}  
+
+## second docker container
+resource "docker_container" "nodered_container-2" {
+  name = "nodered-2"
+  # this is a logical value for referencing only.
+  
+  
+  image = docker_image.nodered_image.latest
+  # this is referenced from the docker image above: docker_image.nodered_image.latest
+  # "latest" can be referenced from other resources as a single image.  This seems to work only with docker 2.15.0
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image
+  
+  # for docker 3.0.2 use 
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/image#read-only
+  # image = docker_image.nodered_image.image_id
+  ##image = docker_image.nodered_image.image_id
+
+  # ports will need to be exposed on this container
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container
+  # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container#nestedblock--ports
+  # internal (Number) Port within the container.; external (Number) Port exposed out of the container. If not given a free random port >= 32768 will be used.
+  # ip (String) IP address/mask that can access this port. Defaults to 0.0.0.0.; protocol (String) Protocol that can be used over this port. Defaults to tcp.
+  ports {
+    # ports is a nested schema
+    internal = 1880
+    # terraform refers to this as a number and not an integer.  1880 is the nodered listening port.  See documentation
+    #external = 1880
+    # if we comment out the external port, docker will automatically choose external port for you
+  }
+}  
+
+
+
+### Generate some ouputs
+
+output "IP_address" {
+# outputs cannot contain spaces in name in newer versions
+  value = docker_container.nodered_container.ip_address
+  description = "the IP address of the nodered container"
+}
+
+output "Container_name" {
+  value = docker_container.nodered_container.name
+  description = "this is the name of the container"
+}
+
+output "IP_address_and_port" {
+# outputs cannot contain spaces in name in newer versions
+  value = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  description = "the IP address and port of the nodered container"
+}
