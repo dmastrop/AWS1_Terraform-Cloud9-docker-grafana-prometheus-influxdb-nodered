754cf455b10d0647a688a922c1693c9ca08036a1
Ubuntu <ubuntu@ip-172-31-11-176.us-west-1.compute.internal>
2023-09-26 01:29:59 +0000

    forgot to comment out the old code line for the value of image_out in image/outputs.tf

--- image/outputs.tf
+++ image/outputs.tf
@@ -2,7 +2,7 @@ output "image_out" {
    #value = docker_image.nodered_image.latest
     # the above is deprecated
     
-    value = docker_image.nodered_image.image_id
+    ## value = docker_image.nodered_image.image_id
     # we could export all attributes with
     # value = docker_image.nodered_image
     
